fadbed20b4932019c5476a95c13376dabd68fa43
trevalkov <52704508+trevalkov@users.noreply.github.com>
2021-07-20 15:12:02 +0100

    Delete apps directory

--- apps/mailClient/main.py
+++
@@ -1,6 +0,0 @@
-import smtplib
-
-# connect to smtp server of choice, port number (25 is the default for smtp)
-server = smtplib.SMTP('smtp.gmail.com', 25)
-
-
--- apps/zoomBot/main.py
+++
@@ -1,1 +0,0 @@
-import keyboard, mouse, time, subprocess
